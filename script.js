5cbefc59e87b6b8d5dd331719e7c2337c6d7c0ca
UZD007 <djdh9515@gmail.com>
2025-04-22 16:47:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let my_data = {
+    name: prompt("Isming nima:"),
+    ID: prompt("Space IDingiz nima:"),
+    Pass: prompt("Space Passwordingiz nima:"),
+}
+
+console.log(my_data.name);
+console.log(my_data.ID);
+console.log(my_data.Pass);
+
+
+
+ 
+
